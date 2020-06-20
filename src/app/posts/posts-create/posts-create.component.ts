acc72c6dd80b46ce2e8ea734c4f3d46ae558cafb
3haran3 <62563541+3haran3@users.noreply.github.com>
2020-06-20 12:22:36 +0530

    Add files via upload

---
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core'; // EventEmitter needed to create events of our own
+// import {Post} from '../post.model';
+import {NgForm} from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-posts-create',
+  templateUrl: './posts-create-component.html',
+  styleUrls: ['./posts-create.component.css']
+}) // this is a decorator which indicates that the below class isn't just a Typescript class but is a compoennt in angular
+export class PostsCreateComponent {
+  newPost = '';
+  enteredContent='';
+  enteredTitle='';
+
+  //if using service, we dont need emitter anymore so commentted below and also remove imports EventEmitter and Output
+  // @Output() postCreatedEvent=new EventEmitter<Post>(); //<Post> says that the data type of the emmitted event will be post. If we dont write
+  // <Post> then it is generic type
+
+  constructor(public postsService:PostsService){
+
+  }
+
+  onClickButtonSavePost(postForm: NgForm) {
+    if(postForm.invalid)
+    {
+      return;
+    }
+    // const post:Post= {
+    //   // title:this.enteredTitle,
+    //   // content:this.enteredContent
+    //   title:postForm.value.titleInput,
+    //   content:postForm.value.contentInput
+    // };
+
+    // this.postCreatedEvent.emit(post); //not using emitter anymore
+    // now using service
+    this.postsService.addPosts(postForm.value.titleInput, postForm.value.contentInput)
+    postForm.reset();
+  }
+}
---
+++ src/app/posts/posts-list/post-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs';
+
+import {Post} from './../post.model';
+import {PostsService} from '../posts.service';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent implements OnInit, OnDestroy {
+
+
+  // posts = [
+  //   {
+  //     title: '1st Post',
+  //     content: 'First Post Content'
+  //   },
+  //   {
+  //     title: '2nd Post',
+  //     content: 'Second Post Content'
+  //   },
+  //   {
+  //     title: '3rd Post',
+  //     content: 'Third Post Content'
+  //   }
+  // ];
+  posts:Post[] = [];
+  private postsSubscription : Subscription;
+  constructor(public postService: PostsService){
+    //saying public postService: PostService is equivalent to setting the value of a local variable postService
+    //to the incoming parameter postService
+    // we save the step of this.postService=postService
+  }
+
+  onDelete(postId:string){
+    console.log('I am here in postlistcomponent.ts and is '+postId);
+    this.postService.deletePost(postId);
+  }
+
+  ngOnInit() {
+    this.postService.getPosts();
+     // subscribe is used to listen to the observable that is returned by getPostUpdatedListener()
+     // subsrcibe can take 3 arguments..all 3 functions
+     // here we shall only use 1st function argument which is executed when new data is emitted.
+     this.postsSubscription=this.postService.getPostUpdatedListener().subscribe((posts:Post[])=>{
+      this.posts=posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
+    // we unsubscribe because incase this component is destroyed in future but the subscription is not then it will cause memory leak.
+  }
+
+}
